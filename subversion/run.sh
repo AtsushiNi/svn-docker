12272b45d69d6a14b265175407a49311b933daf0
AtsushiNi <atsushi9731@gmail.com>
2025-07-18 00:56:09 +0900

    commit

--- subversion/run.sh
+++ subversion/run.sh
@@ -9,9 +9,10 @@ fi
 
 # デフォルトリポジトリがなければ作成
 if [ ! -d "/home/svn/repos/testrepo" ]; then
-    /usr/bin/svnadmin create /home/svn/repos/testrepo
-    chown -R svn:svn /home/svn/repos/testrepo
-    chmod -R 770 /home/svn/repos/testrepo
+    cd /home/svn/repos || exit 1
+    /usr/bin/svnadmin create testrepo
+    chown -R svn:svn testrepo
+    chmod -R 770 testrepo
 fi
 
 exec /usr/bin/svnserve -d --foreground -r /home/svn/repos --listen-port 3690;
