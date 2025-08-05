9637387be421c221fcc73d95bead7ae40f479d10
Niihama Atsushi <45954308+AtsushiNi@users.noreply.github.com>
2025-08-05 23:44:52 +0900

    Update create-repo.sh

--- subversion/create-repo.sh
+++ subversion/create-repo.sh
@@ -3,7 +3,6 @@
 # リポジトリ親ディレクトリがなければ作成
 if [ ! -d "/home/svn/repos" ]; then
     mkdir -p /home/svn/repos
-#    chown -R svn:svn /home/svn/repos
     chmod -R 777 /home/svn
     chmod -R 777 /home/svn/repos
 fi
@@ -13,7 +12,6 @@ rm -rf /home/svn/repos/testrepo
 if [ ! -d "/home/svn/repos/testrepo" ]; then
     cd /home/svn/repos || exit 1
     /usr/bin/svnadmin create testrepo
-    chown -R svn:svn testrepo
     chmod -R 770 testrepo
 fi
 chmod -R 777 /home/svn/repos/testrepo
